165137c0b88d7f6239bbc7f84b05ba3b84a4dd2b
Андрей <andrei_anpilogov@me.com>
2023-04-06 13:33:48 +1000

    Сделал открывающиеся меню

---
+++ source/js/script.js
@@ -0,0 +1,22 @@
+let navMain = document.querySelector('.main-nav');
+let navToggle = document.querySelector('.main-nav__toggle');
+
+navMain.classList.remove('main-nav--nojs');
+
+navToggle.addEventListener('click', function () {
+  if (navMain.classList.contains('main-nav--closed')) {
+    navMain.classList.remove('main-nav--closed');
+    navMain.classList.add('main-nav--opened');
+  } else {
+    navMain.classList.add('main-nav--closed');
+    navMain.classList.remove('main-nav--opened');
+  }
+});
+
+let map = document.querySelector('.search-hotel');
+
+map.classList.remove('search-hotel--nojs');
+
+let headerWrap = document.querySelector('.main-header__wrap');
+
+headerWrap.classList.remove('main-header__wrap--nojs');
